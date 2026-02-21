756c5d734d58db1686e94d3223c61a9f45739bb7
daviddprtma <davidchristianpratama@gmail.com>
2026-02-21 19:33:14 +0700

    new logo for ShowFarm done

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -68,7 +68,11 @@ const Navbar = () => {
               transition={{ duration: 0.5 }}
               className="w-8 h-8 bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg flex items-center justify-center"
             >
-              <span className="text-white font-bold text-sm">SF</span>
+              <img 
+               src='/ShowFarm.jpg'
+                alt='ShowFarm Logo'
+                className="w-5 h-5"
+              />
             </motion.div>
             <span className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
               ShowFarm
